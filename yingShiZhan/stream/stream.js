070440e367aa7a99625345f19ad1bce0e5979c8b
yaojm <yaojm@guahao.com>
2022-03-14 18:02:21 +0800

    fix:可读 可写 双工 转换 流

--- yingShiZhan/stream/stream.js
+++ yingShiZhan/stream/stream.js
@@ -107,3 +107,120 @@
 // jsonLineReader.on('object', obj => {
 //   console.log('pos:', obj)
 // })
+
+// 可读流
+// const stream = require('stream')
+// const util = require('util')
+
+// class MemoryStream extends stream.Readable{
+//   constructor(options = {}) {
+//     // objectMode true 对象流
+//     options.objectMode = true
+//     super(options)
+//   }
+//   _read(size) {
+//     this.push(process.memoryUsage())
+//   }
+// }
+
+// const memoryStream = new MemoryStream();
+// memoryStream.on('readable', () => {
+//   const output = memoryStream.read()
+//   console.log('type:%s, value:%j', typeof output, output)
+// })
+
+// 可写流
+// const stream = require('stream')
+
+// class GreenStream extends stream.Writable {
+//   constructor(options) {
+//     super(options)
+//   }
+//   _write(chunk, encoding, callback) {
+//     // 设置颜色
+//     process.stdout.write('\x1b[32m' + chunk + '\x1b[39m')
+//     console.log('\x1b[1;35mCongratulations!🎉 \x1b[32mYou do it!\x1b[0m')
+//     callback()
+//   }
+// }
+
+// process.stdin.pipe(new GreenStream())
+
+// 双工流（可读 可写）
+// const stream = require('stream')
+
+// class HungryStream extends stream.Duplex {
+//   constructor(options) {
+//     super(options) 
+//     this.waiting = false
+//   }
+//   _write(chunk, encoding, callback) {
+//     this.waiting = false
+//     this.push('\x1b[32m' + chunk + '\x1b[39m')
+//     callback()
+//   }
+//   _read(size) {
+//     if (!this.waiting) {
+//       this.push('feed me data!')
+//       this.waiting = true
+//     }
+//   }
+// }
+
+// const hungryStream = new HungryStream()
+// // stdin => _read 双工流 _write stdout
+// process.stdin.pipe(hungryStream).pipe(process.stdout)
+
+// 转换流 解析数据（转换流是为了改变数据而特殊设计的）
+const fs = require('fs')
+const stream = require('stream')
+
+class CSVParser extends stream.Transform {
+  constructor(options = {}) {
+    super(options)
+    this.value = ''
+    this.headers = []
+    this.values = []
+    this.line = 0
+  }
+  _transform(chunk, encoding, done) {
+    let c, i
+    chunk = chunk.toString()
+    for(i = 0; i < chunk.length; i ++) {
+      c = chunk.charAt(i)
+      if (c === ',') {
+        this.addValue()
+      } else if (c === 'n') {
+        this.addValue()
+        if (this.line > 0) {
+          this.push(JSON.stringify(this.toObject()))
+        }
+        this.values = []
+        this.line ++
+      } else {
+        this.value += c
+      }
+    }
+    done()
+  }
+  toObject() {
+    let i, obj = {}
+    for(i = 0; i < this.headers.length; i++) {
+      obj[this.headers[i]] = this.values[i]
+    }
+    return obj
+  }
+  addValue() {
+    if (this.line === 0) {
+      this.headers.push(this.value)
+    } else {
+      this.values.push(this.value)
+    }
+    this.value = ''
+  }
+}
+
+const parser = new CSVParser()
+fs.createReadStream(__dirname + '/sample.csv')
+  .pipe(parser)
+  .pipe(process.stdout)
